44a11c0561690805156180bdacdf56cc0072e00e
mariusmonton <marius.monton@gmail.com>
2018-10-14 03:36:30 +0200

    missing compile script

---
+++ compile.sh
@@ -0,0 +1,9 @@
+pdflatex -draftmode main
+makeindex main.idx -s StyleInd.ist
+makeglossaries main
+biber main
+pdflatex -draftmode main
+pdflatex main
+
+#pdflatex main.tex && makeglossaries main && bibtex main.aux && pdflatex main.tex && pdflatex main.tex && gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf main.pdf
+#rm main.pdf
