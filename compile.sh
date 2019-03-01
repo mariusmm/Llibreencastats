b6cb4a1f2b7735c04af6fbe823e1d6bad4197d4b
mariusmonton <marius.monton@gmail.com>
2019-03-01 23:27:52 +0100

    option to keep hyperlinks in pdf

--- compile.sh
+++ compile.sh
@@ -5,5 +5,5 @@ biber main
 pdflatex -draftmode main
 pdflatex main
 
-#pdflatex main.tex && makeglossaries main && bibtex main.aux && pdflatex main.tex && pdflatex main.tex && gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf main.pdf
+#pdflatex main.tex && makeglossaries main && bibtex main.aux && pdflatex main.tex && pdflatex main.tex && gs -sDEVICE=pdfwrite -dPrinted=false -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf main.pdf
 #rm main.pdf
